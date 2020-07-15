4c6f0749fbe815e3e07630796814a6bfb67f80aa
Achilles <63617922+Chet1317@users.noreply.github.com>
2020-07-14 21:15:36 -0400

    added seeding to seeds.sql and express and mysql variables to server.js

--- burger_db/schema.sql
+++ burger_db/schema.sql
@@ -7,7 +7,3 @@ burger_name VARCHAR (30),
 devoured BOOLEAN 
 );
 
-INSERT INTO burgers (id, burger_name, devoured) VALUES 
-(1, "chicken bacon burger", false), 
-(0, "mozzerealla burger", false),
-(0, "bacon and cheddar burger", false);
---
+++ burger_db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (id, burger_name, devoured) VALUES 
+(1, "chicken bacon burger", false), 
+(0, "mozzerealla burger", false),
+(0, "bacon and cheddar burger", false);
