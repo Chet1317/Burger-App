6d255981a742b7ed370ccffa1a33c88100ad3b9c
Achilles <63617922+Chet1317@users.noreply.github.com>
2020-07-16 17:14:49 -0400

    created functions to call on orm functions

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,25 @@
-const orm = require("./orm")
+const orm = require( '../config/orm' )
+
+let burgerList 
+
+async function burgerAvailable(){
+    burgerList = await orm.selectAll()
+    return burgerList.filter( burger=>burger.devoured == false );
+}
+
+async function burgerDevoured(){
+    burgerList = await orm.selectAll()
+    return burgerList.filter( burger=>burger.devoured == true );
+}
+
+async function addBurger(){
+    burgerList = await orm.insertOne()
+    return burgerList.filter( name, false );
+}
+
+async function burgerDevour(){
+    burgerList = await orm.updateOne()
+    return burgerList.filter( id, true );
+}
+
+module.exports = { burgerAvailable, burgerDevoured, addBurger, burgerDevour }
